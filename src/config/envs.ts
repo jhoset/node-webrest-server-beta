9f7d986010b2eb1a7e87999d1b606fb7a4fefe2c
Jhoset NG <adm_dng@hotmail.com>
2023-12-22 11:55:18 -0500

    Add default value env. var

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -3,5 +3,5 @@ import { get } from 'env-var';
 
 export const envs = {
     PORT: get('PORT').required().asPortNumber(),
-    PUBLIC_PATH: get('PUBLIC_PATH').required().asString()
+    PUBLIC_PATH: get('PUBLIC_PATH').default('public').asString()
 }
